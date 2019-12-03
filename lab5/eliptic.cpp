cf7ac733e34b865481a8a66728f044b74a67764d
Adrian Setniewski <274433@pw.edu.pl>
2019-12-03 23:00:13 +0100

    Proper formatting.

--- lab5/eliptic.cpp
+++ lab5/eliptic.cpp
@@ -1,10 +1,9 @@
 #include <unistd.h>
-#include <stdlib.h> //for exit(0);
+#include <stdlib.h>
 #include <fstream>
 #include <iostream>
 #include <vector>
 #include <cstdio>
-// OpenSSL includes
 #include <openssl/obj_mac.h>
 #include <openssl/ec.h>
 #include <openssl/ecdsa.h> 
@@ -26,82 +25,82 @@ typedef struct EC_Params {
 
 void handleErrors() {
 	std::cerr << "An error has been encountered!" << std::endl;
-	
+
 }
 
 
-EC_GROUP *create_curve(EC_Params& params){
+EC_GROUP* create_curve(EC_Params& params) {
 	std::cout << std::endl << std::endl << std::endl;
 	std::cout << "\n1) Creating group" << std::endl;
-    BN_CTX *ctx;
-    EC_GROUP *curve;
-    BIGNUM *a, *b, *p, *order, *x, *y;
-    EC_POINT *generator;
+	BN_CTX* ctx;
+	EC_GROUP* curve;
+	BIGNUM* a, * b, * p, * order, * x, * y;
+	EC_POINT* generator;
 
-    /* Binary data for the curve parameters */
-    unsigned char a_bin[28] =
-        {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
-            0xFF,0xFF,0xFF,0xFF,0xFF,0xFE,0xFF,0xFF,0xFF,0xFF,
-            0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFE};
-    unsigned char b_bin[28] =
-        {0xB4,0x05,0x0A,0x85,0x0C,0x04,0xB3,0xAB,0xF5,0x41,
-            0x32,0x56,0x50,0x44,0xB0,0xB7,0xD7,0xBF,0xD8,0xBA,
-            0x27,0x0B,0x39,0x43,0x23,0x55,0xFF,0xB4};
-    unsigned char p_bin[28] =
-        {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
-            0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x00,
-            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01};
-    unsigned char order_bin[28] =
-        {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
-            0xFF,0xFF,0xFF,0xFF,0x16,0xA2,0xE0,0xB8,0xF0,0x3E,
-            0x13,0xDD,0x29,0x45,0x5C,0x5C,0x2A,0x3D };
-    unsigned char x_bin[28] =
-        {0xB7,0x0E,0x0C,0xBD,0x6B,0xB4,0xBF,0x7F,0x32,0x13,
-            0x90,0xB9,0x4A,0x03,0xC1,0xD3,0x56,0xC2,0x11,0x22,
-            0x34,0x32,0x80,0xD6,0x11,0x5C,0x1D,0x21};
-    unsigned char y_bin[28] =
-        {0xbd,0x37,0x63,0x88,0xb5,0xf7,0x23,0xfb,0x4c,0x22,
-            0xdf,0xe6,0xcd,0x43,0x75,0xa0,0x5a,0x07,0x47,0x64,
-            0x44,0xd5,0x81,0x99,0x85,0x00,0x7e,0x34};
+	/* Binary data for the curve parameters */
+	unsigned char a_bin[28] =
+	{ 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+		0xFF,0xFF,0xFF,0xFF,0xFF,0xFE,0xFF,0xFF,0xFF,0xFF,
+		0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFE };
+	unsigned char b_bin[28] =
+	{ 0xB4,0x05,0x0A,0x85,0x0C,0x04,0xB3,0xAB,0xF5,0x41,
+		0x32,0x56,0x50,0x44,0xB0,0xB7,0xD7,0xBF,0xD8,0xBA,
+		0x27,0x0B,0x39,0x43,0x23,0x55,0xFF,0xB4 };
+	unsigned char p_bin[28] =
+	{ 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+		0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x00,
+		0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01 };
+	unsigned char order_bin[28] =
+	{ 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+		0xFF,0xFF,0xFF,0xFF,0x16,0xA2,0xE0,0xB8,0xF0,0x3E,
+		0x13,0xDD,0x29,0x45,0x5C,0x5C,0x2A,0x3D };
+	unsigned char x_bin[28] =
+	{ 0xB7,0x0E,0x0C,0xBD,0x6B,0xB4,0xBF,0x7F,0x32,0x13,
+		0x90,0xB9,0x4A,0x03,0xC1,0xD3,0x56,0xC2,0x11,0x22,
+		0x34,0x32,0x80,0xD6,0x11,0x5C,0x1D,0x21 };
+	unsigned char y_bin[28] =
+	{ 0xbd,0x37,0x63,0x88,0xb5,0xf7,0x23,0xfb,0x4c,0x22,
+		0xdf,0xe6,0xcd,0x43,0x75,0xa0,0x5a,0x07,0x47,0x64,
+		0x44,0xd5,0x81,0x99,0x85,0x00,0x7e,0x34 };
 
-    /* Set up the BN_CTX */
-    if(NULL == (ctx = BN_CTX_new())) handleErrors();
+	/* Set up the BN_CTX */
+	if (NULL == (ctx = BN_CTX_new())) handleErrors();
 	if (NULL == (a = BN_bin2bn(a_bin, 28, NULL))) handleErrors();
 	if (NULL == (b = BN_bin2bn(b_bin, 28, NULL))) handleErrors();
 	if (NULL == (p = BN_bin2bn(p_bin, 28, NULL))) handleErrors();
 	if (NULL == (order = BN_bin2bn(order_bin, 28, NULL))) handleErrors();
 	if (NULL == (x = BN_bin2bn(x_bin, 28, NULL))) handleErrors();
 	if (NULL == (y = BN_bin2bn(y_bin, 28, NULL))) handleErrors();
-    /* Set the values for the various parameters */
-    //if(NULL == (a = BN_bin2bn(params.a.data(), params.a.size(), NULL))) handleErrors();
-    //if(NULL == (b = BN_bin2bn(params.b.data(), params.b.size(), NULL))) handleErrors();
-    //if(NULL == (p = BN_bin2bn(params.p.data(), params.p.size(), NULL))) handleErrors();
-    //if(NULL == (order = BN_bin2bn(params.order.data(), params.order.size(), NULL))) handleErrors();
-    //if(NULL == (x = BN_bin2bn(params.x.data(), params.x.size(), NULL))) handleErrors();
-    //if(NULL == (y = BN_bin2bn(params.y.data(), params.y.size(), NULL))) handleErrors();
+	/* Set the values for the various parameters */
+	//if(NULL == (a = BN_bin2bn(params.a.data(), params.a.size(), NULL))) handleErrors();
+	//if(NULL == (b = BN_bin2bn(params.b.data(), params.b.size(), NULL))) handleErrors();
+	//if(NULL == (p = BN_bin2bn(params.p.data(), params.p.size(), NULL))) handleErrors();
+	//if(NULL == (order = BN_bin2bn(params.order.data(), params.order.size(), NULL))) handleErrors();
+	//if(NULL == (x = BN_bin2bn(params.x.data(), params.x.size(), NULL))) handleErrors();
+	//if(NULL == (y = BN_bin2bn(params.y.data(), params.y.size(), NULL))) handleErrors();
 
-    /* Create the curve */
-    if(NULL == (curve = EC_GROUP_new_curve_GFp(p, a, b, ctx))) handleErrors();
+	/* Create the curve */
+	if (NULL == (curve = EC_GROUP_new_curve_GFp(p, a, b, ctx))) handleErrors();
 
-    /* Create the generator */
-    if(NULL == (generator = EC_POINT_new(curve))) handleErrors();
-    if(1 != EC_POINT_set_affine_coordinates_GFp(curve, generator, x, y, ctx))
-        handleErrors();
+	/* Create the generator */
+	if (NULL == (generator = EC_POINT_new(curve))) handleErrors();
+	if (1 != EC_POINT_set_affine_coordinates_GFp(curve, generator, x, y, ctx))
+		handleErrors();
 
-    /* Set the generator and the order */
-    if(1 != EC_GROUP_set_generator(curve, generator, order, NULL))
-        handleErrors();
+	/* Set the generator and the order */
+	if (1 != EC_GROUP_set_generator(curve, generator, order, NULL))
+		handleErrors();
 
-    EC_POINT_free(generator);
-    BN_free(y);
-    BN_free(x);
-    BN_free(order);
-    BN_free(p);
-    BN_free(b);
-    BN_free(a);
-    BN_CTX_free(ctx); 
+	EC_POINT_free(generator);
+	BN_free(y);
+	BN_free(x);
+	BN_free(order);
+	BN_free(p);
+	BN_free(b);
+	BN_free(a);
+	BN_CTX_free(ctx);
 
-    return curve;
+	return curve;
 }
 
 struct DHEntity {
@@ -115,7 +114,7 @@ struct DHEntity {
 
 std::vector<unsigned char> getParam(std::string paramName) {
 
-	std::cout << std::endl << "Please specify eliptic curve parameters: " << paramName <<  std::endl;
+	std::cout << std::endl << "Please specify eliptic curve parameters: " << paramName << std::endl;
 
 	long long unsigned int in;
 	std::cin >> in;
@@ -130,7 +129,7 @@ std::vector<unsigned char> getParam(std::string paramName) {
 	for (unsigned int ii = 0; ii < sizeof(long long unsigned int); ii++) {
 		printf("%02x", a[ii]);
 	}
-	
+
 	return a;
 }
 
@@ -152,11 +151,11 @@ void printSecret(DHEntity* a, DHEntity* b, bool invert = 0) {
 	std::cout << std::endl << std::endl;
 
 	len = ECDH_compute_key(secret, len, b->pub, a->keyPair, NULL);
-	std::cout << "=============SECRET BEGIN==============" <<std::endl;
+	std::cout << "=============SECRET BEGIN==============" << std::endl;
 	for (i = 0; i < len; i++)
 		printf("%02X", secret[i]);
 	std::cout << std::endl << "=============+SECRET  END+==============" << std::endl;
-	std::cout << std::endl << std::endl ;
+	std::cout << std::endl << std::endl;
 	OPENSSL_free(secret);
 
 }
@@ -202,12 +201,12 @@ void signAndVerify(EC_GROUP* curve) {
 	EC_KEY_check_key(key);
 	// We need to store the private key in EVP_PKEY
 	EVP_PKEY* pkey = EVP_PKEY_new();
-	
+
 	BIGNUM const* prv = EC_KEY_get0_private_key(key);
 	EC_POINT const* pub = EC_KEY_get0_public_key(key);
 	printf("%s\n", BN_bn2dec(prv));
 	std::cout << (pub) << std::endl;
-	
+
 	unsigned char* hashedMSG = "NOT HASHED MESSAGE";
 	ECDSA_SIG* sign = ECDSA_do_sign(hashedMSG, strlen((char*)hashedMSG), key);
 	if (sign == 0) {
@@ -216,11 +215,11 @@ void signAndVerify(EC_GROUP* curve) {
 	}
 
 	std::string ret = (ECDSA_do_verify(hashedMSG, strlen((char*)hashedMSG), sign, key) ? "true" : "false");
-	std::cout << "Verification :" << ret  << std::endl;
+	std::cout << "Verification :" << ret << std::endl;
 }
 
 
- 
+
 int main(int argc, char** argv) {
 	EC_Params params;
 	params.a = getParam("a");
@@ -238,7 +237,7 @@ int main(int argc, char** argv) {
 	DHEntity* alice = generateKey(curve);
 	printSecret(alice, bob);
 	printSecret(bob, alice, true);
-	
+
 	signAndVerify(curve);
 }
 
